0aa8e9b7b03450d7b8c555eeb9f0761cb60eeef6
LeviScoffie <kithinjimiriti4@gmail.com>
2023-05-23 22:25:51 +0300

    new commit

--- app/api/dummy/brand.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'Liquidity Fantasy Game',
-  desc: 'Liquidity Fantasy Game',
-  prefix: 'fantasy ga,e',
-  footerText: 'Liquidity Fantasy Game All rights reserved 2023',
-  logoText: 'Enlite Starter',
-};
